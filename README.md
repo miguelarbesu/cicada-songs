c500a0b0cc4bbc44bbff33157547919c187cd22f
Miguel Arbesú <miguelarbesu@gmail.com>
2022-08-18 11:13:07 +0200

    complete project basics

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cicada songs
+
+![License](https://img.shields.io/github/license/miguelarbesu/cicada-songs)
+
+---
+
+A small summer experiment to analyze audio files of cicadas singing.
+
+## Copyright
+
+Copyright (c) 2022, Miguel Arbesú
+
--- scripts/convert.sh
+++ scripts/convert.sh
@@ -5,6 +5,6 @@
 jupyter nbconvert --to markdown notebooks/0-finding-periodicity.ipynb
 cp notebooks/0-finding-periodicity_files/* figures/
 
-# export figures only
+# export md and figures
 jupyter nbconvert --to html notebooks/0-finding-periodicity.ipynb
 cp notebooks/0-finding-periodicity.html ~/code/repos/miguelarbesu.xyz/content/post/cicada-songs-finding-periodicity/
