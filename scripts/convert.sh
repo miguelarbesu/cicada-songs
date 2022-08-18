18e27bd91d693e9c19c04113fac28d5d90b778a4
Miguel Arbesú <miguelarbesu@gmail.com>
2022-08-18 11:06:46 +0200

    initial commit

---
+++ scripts/convert.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# to html
+jupyter nbconvert --to markdown notebooks/0-finding-periodicity.ipynb
+cp notebooks/0-finding-periodicity_files/* figures/
+
+# export figures only
+jupyter nbconvert --to html notebooks/0-finding-periodicity.ipynb
+cp notebooks/0-finding-periodicity.html ~/code/repos/miguelarbesu.xyz/content/post/cicada-songs-finding-periodicity/
